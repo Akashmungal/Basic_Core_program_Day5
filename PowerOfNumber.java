5ada156237d6793fb7ac187bd4bfbdc44ace0c91
Akashmungal <mungalakash92@gmail.com>
2022-03-08 04:03:06 +0530

    Display Power Of Two Till Number

---
+++ PowerOfNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PowerOfNumber {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=scan.nextInt();
+
+        System.out.println("Power of 2^" +num+ " is: "+(Math.pow(2, num)));
+        System.out.println();
+        System.out.println("Power of 2 till " + num);
+
+        for(int i=1; i<=num; i++)
+        {
+            System.out.println("Power of 2^"+i+" is: " +(Math.pow(2, i)));
+        }
+    }
+
+}
